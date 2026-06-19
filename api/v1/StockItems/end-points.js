d69787096f5bd479e37f319d7505ed59c44c9910
keshavsoft <nknnkumar@yahoo.com>
2026-06-19 22:48:13 +0530

    del to start as new repo

--- api/v1/StockItems/end-points.js
+++ api/v1/StockItems/end-points.js
@@ -2,6 +2,7 @@ import express from 'express';
 
 import funcFromshowAll from './showAll/controller.js';
 import funcFrominsertGenPk from './insertGenPk/controller.js';
+import funcFromfind from './find/controller.js';
 
 const tableName = "StockItems";
 const tablePath = "Data/StockItems.json";
@@ -11,5 +12,6 @@ const router = express.Router();
 
 router.get('/showAll', (req, res) => funcFromshowAll({ req, res, inTablePath: tablePath }));
 router.post('/insertGenPk', express.json(), (req, res) => funcFrominsertGenPk({ req, res, inTablePath: tablePath, inConfigPath: configPath }));
+router.delete('/find/:pk', (req, res) => funcFromfind({ req, res, inTablePath: tablePath }));
 
 export { router };
